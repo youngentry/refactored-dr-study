2114d57fa1595e4e93cee35604549bd8e2548b9f
ABizCho <he1236@ajou.ac.kr>
2024-07-22 14:29:38 +0900

    fe-chore: [디렉토리 구조 개선] app하위 구조 변경

--- FE/src/app/login/find-account/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import FindAccountTemplate from '@/components/template/login/FindAccountTemplate';
-
-const FindAccountPage = () => {
-  return <FindAccountTemplate />;
-};
-
-export default FindAccountPage;
--- FE/src/app/study-group/[study_id]/post/[post_id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import StudyGroupPostDetailTemplate from '@/components/template/study-group/StudyGroupPostDetailTemplate';
-
-const StudyGroupPostDetailPage = () => {
-  return <StudyGroupPostDetailTemplate />;
-};
-
-export default StudyGroupPostDetailPage;
