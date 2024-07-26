d5e532fb2285119b954fcfe457e67cca2e76c29a
ABizCho <he1236@ajou.ac.kr>
2024-07-26 15:42:42 +0900

    fe-feat(-newModerator-stepBlock): scriptFunction 타입 작성 중

--- fe_dr_study/src/app/moderator/new/page.tsx
+++ fe_dr_study/src/app/moderator/new/page.tsx
@@ -15,31 +15,76 @@ type BlockType =
     | 'block_command_letParticipant_speak'
     | 'block_string_concat'
     | 'block_string_input'
-    | 'block_getParticiapntRecord_recent';
+    | 'block_getParticipantRecord_recent';
 
+type ScriptFunctionType =
+    | 'phase( *$[int phase_num] ) { $[ ...validChildBlockFunctions ] }'
+    | 'loop( $[int loop_num] ) { $[ ...validChildBlockFunctions ] }'
+    | 'let_participant_speak( $[int pid], $[int time] );'
+    | 'let_ai_speak( get_answer_from_gpt_query( concat_string( $[ ...validChildBlockFunctions ] ) ) );'
+    | 'string_input("$[string inputStr]")'
+    | 'get_recent_participant_speak( $[int prev_num] )';
+
+// const ScriptFunctionList = {
+//     phase: ,
+//     loop: ,
+
+// }
 interface Block {
     id: string;
     type: BlockType;
     content: string;
     children?: Block[];
+    scriptFunction: ScriptFunctionType;
 }
-
 const blocks: Block[] = [
-    { id: uuidv4(), type: 'block_flow_phase_1', content: '1단계' },
-    { id: uuidv4(), type: 'block_flow_phase_2', content: '2단계' },
-    { id: uuidv4(), type: 'block_flow_phase_3', content: '3단계' },
-    { id: uuidv4(), type: 'block_flow_phase_4', content: '4단계' },
-    { id: uuidv4(), type: 'block_flow_loop', content: '반복 블록' },
-    { id: uuidv4(), type: 'block_command_queryToGPT', content: 'GPT 블록' },
+    {
+        id: uuidv4(),
+        type: 'block_flow_phase_1',
+        content: '1단계',
+        scriptFunction: '',
+    },
+    {
+        id: uuidv4(),
+        type: 'block_flow_phase_2',
+        content: '2단계',
+        scriptFunction: '',
+    },
+    {
+        id: uuidv4(),
+        type: 'block_flow_phase_3',
+        content: '3단계',
+        scriptFunction: '',
+    },
+    {
+        id: uuidv4(),
+        type: 'block_flow_phase_4',
+        content: '4단계',
+        scriptFunction: '',
+    },
+    {
+        id: uuidv4(),
+        type: 'block_flow_loop',
+        content: '반복 블록',
+        scriptFunction: '',
+    },
+    {
+        id: uuidv4(),
+        type: 'block_command_queryToGPT',
+        content: 'GPT 블록',
+        scriptFunction: '',
+    },
     {
         id: uuidv4(),
         type: 'block_command_letParticipant_speak',
         content: '발화 시작',
+        scriptFunction: '',
     },
     {
         id: uuidv4(),
-        type: 'block_getParticiapntRecord_recent',
+        type: 'block_getParticipantRecord_recent',
         content: '직전 발화자의 발화 내용',
+        scriptFunction: '',
     },
 ];
 
@@ -70,145 +115,15 @@ const validChildBlocks: Record<BlockType, BlockType[]> = {
     ],
     block_command_queryToGPT: [
         'block_string_concat',
-        'block_getParticiapntRecord_recent',
+        'block_getParticipantRecord_recent',
     ],
     block_command_letParticipant_speak: [],
     block_string_concat: [
         'block_string_input',
-        'block_getParticiapntRecord_recent',
+        'block_getParticipantRecord_recent',
     ],
     block_string_input: [],
-    block_getParticiapntRecord_recent: [],
-};
-
-const DraggableBlock: React.FC<{ block: Block }> = ({ block }) => {
-    const [{ isDragging }, drag] = useDrag(() => ({
-        type: 'BLOCK',
-        item: block,
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    }));
-
-    return (
-        <div
-            ref={drag}
-            className={`UNIT-BLOCK w-full min-h-14 p-2 cursor-pointer ${isDragging ? 'opacity-50' : 'opacity-100'}`}
-            style={{ backgroundColor: getBlockColor(block.type) }}
-        >
-            {block.content}
-        </div>
-    );
-};
-
-const getBlockColor = (type: BlockType): string => {
-    switch (type) {
-        case 'block_flow_phase_1':
-        case 'block_flow_phase_2':
-        case 'block_flow_phase_3':
-        case 'block_flow_phase_4':
-            return 'red';
-        case 'block_flow_loop':
-            return 'darkred';
-        case 'block_command_queryToGPT':
-            return 'violet';
-        case 'block_command_letParticipant_speak':
-            return 'blue';
-        case 'block_getParticiapntRecord_recent':
-            return 'gray';
-        default:
-            return 'gray';
-    }
-};
-
-const DroppableBlock: React.FC<{
-    block: Block;
-    onDrop: (block: Block, targetBlock: Block) => void;
-    onDelete: (block: Block) => void;
-}> = ({ block, onDrop, onDelete, children }) => {
-    const [{ isOver, canDrop }, drop] = useDrop(() => ({
-        accept: 'BLOCK',
-        drop: (item: Block, monitor) => {
-            if (!monitor.didDrop()) {
-                if (validChildBlocks[block.type].includes(item.type)) {
-                    onDrop({ ...item, id: uuidv4() }, block);
-                } else {
-                    alert(
-                        `유효하지 않은 블록입니다. ${block.type} 블록에는 ${validChildBlocks[block.type].join(', ')} 블록만 추가할 수 있습니다.`,
-                    );
-                }
-            }
-        },
-        collect: (monitor) => ({
-            isOver: monitor.isOver(),
-            canDrop: monitor.canDrop(),
-        }),
-        canDrop: (item: Block) =>
-            validChildBlocks[block.type].includes(item.type),
-    }));
-
-    const handleDelete = () => {
-        if (window.confirm('정말 삭제하시겠습니까?')) {
-            onDelete(block);
-        }
-    };
-
-    return (
-        <div
-            ref={drop}
-            className={`UNIT-BLOCK  animate-popIn relative ${isOver ? 'bg-dr-dark-200' : ''} border-opacity-0 border-dr-coral-300 ${canDrop ? 'border-2 border-opacity-100' : ''} rounded-md w-full my-2 min-h-20 h-auto transition-all duration-300`}
-            style={{ backgroundColor: getBlockColor(block.type) }}
-        >
-            {' '}
-            <div className="flex flex-row h-auto gap-2 justify-between">
-                <div className="HEAD rounded-tl-md rounded-br-md bg-black h-full min-w-[100px] p-1">
-                    {block.content}
-                </div>
-                <div className="CHILDREN h-auto w-full">{children}</div>
-                <button
-                    className="w-6 h-6 m-1 p-1  rounded-full hover:text-gray-500 transition-colors duration-200"
-                    onClick={handleDelete}
-                >
-                    <FaTrash />
-                </button>
-            </div>
-        </div>
-    );
-};
-
-const DroppableArea: React.FC<{
-    onDrop: (block: Block) => void;
-    hasPhaseBlock: boolean;
-    onDelete: (block: Block) => void;
-}> = ({ onDrop, hasPhaseBlock, onDelete, children }) => {
-    const [{ isOver, canDrop }, drop] = useDrop(() => ({
-        accept: 'BLOCK',
-        drop: (item: Block, monitor) => {
-            if (!monitor.didDrop()) {
-                if (hasPhaseBlock || item.type.startsWith('block_flow_phase')) {
-                    onDrop({ ...item, id: uuidv4() });
-                } else {
-                    alert('단계 블록을 먼저 추가해야 합니다.');
-                }
-            }
-        },
-        collect: (monitor) => ({
-            isOver: monitor.isOver(),
-            canDrop: monitor.canDrop(),
-        }),
-        canDrop: (item: Block) =>
-            hasPhaseBlock || item.type.startsWith('block_flow_phase'),
-    }));
-
-    return (
-        <div
-            ref={drop}
-            className={`DROPPABLE_AREA bg-dr-dark-200 w-full h-auto min-h-full flex flex-col px-2 py-2 ${isOver ? 'bg-dr-dark-100' : ''} border-opacity-0 border-dr-coral-300 ${canDrop ? 'border-2 border-opacity-100' : ''} transition-all duration-300`}
-        >
-            {isOver ? '내려놓을 수 있음' : '블록 끌어오셈'}
-            {children}
-        </div>
-    );
+    block_getParticipantRecord_recent: [],
 };
 
 const CreateModeratorPage: React.FC = () => {
@@ -241,7 +156,6 @@ const CreateModeratorPage: React.FC = () => {
                 prevBlocks.map((b) => addChildToBlock(b, newBlock)),
             );
         } else {
-            // 최상위 블록으로 추가
             setDroppedBlocks((prevBlocks) => [...prevBlocks, newBlock]);
         }
     };
@@ -334,4 +248,135 @@ const CreateModeratorPage: React.FC = () => {
     );
 };
 
+const DraggableBlock: React.FC<{ block: Block }> = ({ block }) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: 'BLOCK',
+        item: block,
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    }));
+
+    return (
+        <div
+            ref={drag}
+            className={`UNIT-BLOCK w-full min-h-14 p-2 cursor-pointer ${isDragging ? 'opacity-50' : 'opacity-100'}`}
+            style={{ backgroundColor: getBlockColor(block.type) }}
+        >
+            {block.content}
+        </div>
+    );
+};
+
+const getBlockColor = (type: BlockType): string => {
+    switch (type) {
+        case 'block_flow_phase_1':
+        case 'block_flow_phase_2':
+        case 'block_flow_phase_3':
+        case 'block_flow_phase_4':
+            return 'red';
+        case 'block_flow_loop':
+            return 'darkred';
+        case 'block_command_queryToGPT':
+            return 'violet';
+        case 'block_command_letParticipant_speak':
+            return 'blue';
+        case 'block_getParticipantRecord_recent':
+            return 'gray';
+        default:
+            return 'gray';
+    }
+};
+
+const DroppableBlock: React.FC<{
+    block: Block;
+    onDrop: (block: Block, targetBlock: Block) => void;
+    onDelete: (block: Block) => void;
+}> = ({ block, onDrop, onDelete, children }) => {
+    const [{ isOver, canDrop }, drop] = useDrop(() => ({
+        accept: 'BLOCK',
+        drop: (item: Block, monitor) => {
+            if (!monitor.didDrop()) {
+                if (validChildBlocks[block.type].includes(item.type)) {
+                    onDrop({ ...item, id: uuidv4() }, block);
+                } else {
+                    alert(
+                        `유효하지 않은 블록입니다. ${block.type} 블록에는 ${validChildBlocks[block.type].join(', ')} 블록만 추가할 수 있습니다.`,
+                    );
+                }
+            }
+        },
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+        canDrop: (item: Block) =>
+            validChildBlocks[block.type].includes(item.type),
+    }));
+
+    const handleDelete = () => {
+        if (window.confirm('정말 삭제하시겠습니까?')) {
+            onDelete(block);
+        }
+    };
+
+    return (
+        <div
+            ref={drop}
+            className={`UNIT-BLOCK animate-popIn relative ${isOver ? 'bg-dr-dark-200' : ''} border-opacity-0 border-dr-coral-300 ${canDrop ? 'border-2 border-opacity-100' : ''} rounded-md w-full my-2 min-h-20 h-auto transition-all duration-300`}
+            style={{ backgroundColor: getBlockColor(block.type) }}
+        >
+            {' '}
+            <div className="flex flex-row h-auto gap-2 justify-between">
+                <div className="HEAD rounded-tl-md rounded-br-md bg-black h-full min-w-[100px] p-1">
+                    {block.content}
+                </div>
+                <div className="CHILDREN h-auto w-full">{children}</div>
+                <button
+                    className="w-6 h-6 m-1 p-1  rounded-full hover:text-gray-500 transition-colors duration-200"
+                    onClick={handleDelete}
+                >
+                    <FaTrash />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+const DroppableArea: React.FC<{
+    onDrop: (block: Block) => void;
+    hasPhaseBlock: boolean;
+    onDelete: (block: Block) => void;
+}> = ({ onDrop, hasPhaseBlock, onDelete, children }) => {
+    const [{ isOver, canDrop }, drop] = useDrop(() => ({
+        accept: 'BLOCK',
+        drop: (item: Block, monitor) => {
+            if (!monitor.didDrop()) {
+                if (hasPhaseBlock || item.type.startsWith('block_flow_phase')) {
+                    onDrop({ ...item, id: uuidv4() });
+                } else {
+                    alert('단계 블록을 먼저 추가해야 합니다.');
+                }
+            }
+        },
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+        canDrop: (item: Block) =>
+            hasPhaseBlock || item.type.startsWith('block_flow_phase'),
+    }));
+
+    return (
+        <div
+            ref={drop}
+            className={`DROPPABLE_AREA bg-dr-dark-200 w-full h-auto min-h-full flex flex-col px-2 py-2 ${isOver ? 'bg-dr-dark-100' : ''} border-opacity-0 border-dr-coral-300 ${canDrop ? 'border-2 border-opacity-100' : ''} transition-all duration-300`}
+        >
+            {isOver ? '내려놓을 수 있음' : '블록 끌어오셈'}
+            {children}
+        </div>
+    );
+};
+
 export default CreateModeratorPage;
+//
