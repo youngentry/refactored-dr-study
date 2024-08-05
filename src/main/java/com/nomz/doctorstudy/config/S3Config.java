09033d98632923ad5dc7e28da1e2336243ce5a89
장철현 <wkdcjfgus000@naver.com>
2024-08-05 15:59:43 +0900

    Update src/main/java/com/nomz/doctorstudy/config/S3Config.java

--- src/main/java/com/nomz/doctorstudy/config/S3Config.java
+++ src/main/java/com/nomz/doctorstudy/config/S3Config.java
@@ -11,10 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Lazy;
 
 @Slf4j
 @Configuration
+@Lazy
 public class S3Config {
 
     // @Value("${cloud.accesskey1}")
