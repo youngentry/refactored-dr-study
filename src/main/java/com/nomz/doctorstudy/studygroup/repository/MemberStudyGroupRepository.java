3c86f490bfd65db5e7cd83b043dd4d480013bb7c
kyoungmopark <kyoungmop@gmail.com>
2024-08-12 14:40:05 +0900

    be-fix : my-group 조회 시 그룹 탈퇴 여부, 그룹 삭제 여부, 멤버 탈퇴 여부 검사하도록 수정

--- src/main/java/com/nomz/doctorstudy/studygroup/repository/MemberStudyGroupRepository.java
+++ src/main/java/com/nomz/doctorstudy/studygroup/repository/MemberStudyGroupRepository.java
@@ -4,12 +4,21 @@ import com.nomz.doctorstudy.studygroup.entity.MemberStudyGroup;
 import com.nomz.doctorstudy.studygroup.entity.MemberStudyGroupId;
 import com.nomz.doctorstudy.studygroup.entity.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberStudyGroupRepository extends JpaRepository<MemberStudyGroup, MemberStudyGroupId> {
     List<MemberStudyGroup> findByMemberStudyGroupIdStudyGroupIdAndIsLeavedFalse(Long studyGroupId);
-    List<MemberStudyGroup> findByMemberId(Long memberId);
+    @Query("SELECT msg FROM MemberStudyGroup msg " +
+            "JOIN msg.studyGroup sg " +
+            "JOIN msg.member m " +
+            "WHERE m.id = :memberId " +
+            "AND msg.isLeaved = false " +
+            "AND sg.isDeleted = false " +
+            "AND m.isLeaved = false")
+    List<MemberStudyGroup> findByMemberId(@Param("memberId") Long memberId);
     Integer countByStudyGroupAndIsLeavedFalse(StudyGroup studyGroup);
     boolean existsByMemberStudyGroupIdMemberIdAndMemberStudyGroupIdStudyGroupIdAndIsLeavedFalse(Long memberId, Long studyGroupId);
 
