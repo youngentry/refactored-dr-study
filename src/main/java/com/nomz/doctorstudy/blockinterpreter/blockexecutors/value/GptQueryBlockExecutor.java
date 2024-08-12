b616368942cf5ff60c17572dec570b9a3f3e0d75
신재민 <woals1488@naver.com>
2024-08-13 00:46:49 +0900

    be-feature: GPT 블록에 문맥정보 포함 추가

--- src/main/java/com/nomz/doctorstudy/blockinterpreter/blockexecutors/value/GptQueryBlockExecutor.java
+++ src/main/java/com/nomz/doctorstudy/blockinterpreter/blockexecutors/value/GptQueryBlockExecutor.java
@@ -4,6 +4,7 @@ import com.nomz.doctorstudy.api.ExternalApiCallService;
 import com.nomz.doctorstudy.blockinterpreter.ProcessContext;
 import com.nomz.doctorstudy.blockinterpreter.ThreadProcessContext;
 import com.nomz.doctorstudy.blockinterpreter.blockexecutors.BlockExecutor;
+import com.nomz.doctorstudy.blockinterpreter.blockexecutors.BlockVariable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -25,17 +26,32 @@ public class GptQueryBlockExecutor extends BlockExecutor {
     protected Object executeAction(List<Object> args) {
         String query = (String) args.get(0);
 
-        String gptHistory = threadProcessContext.get().getGptContext().getHistory();
-        String prePrompt = threadProcessContext.get().getGptContext().getPrePrompt();
+        ProcessContext processContext = threadProcessContext.get();
+
+        String gptHistory = processContext.getGptContext().getHistory();
+        String prePrompt = processContext.getConferenceContext().getPrePrompt();
+        String subject = processContext.getConferenceContext().getSubject();
+
+        StringBuilder contextBuilder = new StringBuilder();
+        int numOfParticipant = processContext.getNumOfParticipant();
+        for (int i=1; i<=numOfParticipant; i++) {
+            String participantName = processContext.getParticipantName(i);
+            contextBuilder.append("<").append(i).append("번 참여자=").append(participantName).append(">");
+        }
+        contextBuilder.append("<").append("주제=").append(subject).append(">");
+        int phase = (int) processContext.getVariable(BlockVariable.CURRENT_PHASE.getToken());
+        contextBuilder.append("<").append("페이즈(단계)=").append(processContext).append(phase);
 
         StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("[History Start]").append(gptHistory).append("[History End]");
         queryBuilder.append("[PrePrompt Start]").append(prePrompt).append("[PrePrompt End]");
+        queryBuilder.append("[Gpt Query History Start]").append(gptHistory).append("[Gpt Query History End]");
+        queryBuilder.append("[Context Start]").append(contextBuilder).append("[Context End]");
+        queryBuilder.append("[Current Query Start]").append(query).append("[Current Query End]");
 
         String answer = externalApiCallService.gpt(queryBuilder.toString());
-        threadProcessContext.get().addGptHistory(query, answer);
+        processContext.addGptHistory(query, answer);
 
-        log.debug("[GPT]query={}answer={}", queryBuilder.toString(), answer);
+        log.debug("[GPT]\n[Query]\n{}\n[Answer]\n{}", queryBuilder, answer);
 
         return answer;
     }
