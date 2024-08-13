5ebf906fbf17de7ed60c7798e2b8c6d14ca56579
kyoungmopark <kyoungmop@gmail.com>
2024-08-13 10:57:32 +0900

    be-fix : Article, Commment에 Validation 추가

--- src/main/java/com/nomz/doctorstudy/article/request/CommentRequest.java
+++ src/main/java/com/nomz/doctorstudy/article/request/CommentRequest.java
@@ -1,6 +1,8 @@
 package com.nomz.doctorstudy.article.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +14,8 @@ import lombok.ToString;
 @ToString
 public class CommentRequest {
     @Schema(description = "댓글 내용", example = "확인했습니다!")
+    @NotNull(message = "댓글 내용은 반드시 포함되어야 합니다.")
+    @Size(min = 1, max =256, message = "내용은 1자 이상 64자 이하여야 합니다.")
     private String content;
 
 }
