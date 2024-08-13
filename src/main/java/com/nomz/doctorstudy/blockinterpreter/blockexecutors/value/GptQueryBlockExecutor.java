bcd5e6b303beb93df632078de3bdc2a3c669b61c
신재민 <woals1488@naver.com>
2024-08-13 13:23:13 +0900

    be-fix: GPT 쿼리 문맥 구성 수정

--- src/main/java/com/nomz/doctorstudy/blockinterpreter/blockexecutors/value/GptQueryBlockExecutor.java
+++ src/main/java/com/nomz/doctorstudy/blockinterpreter/blockexecutors/value/GptQueryBlockExecutor.java
@@ -33,25 +33,28 @@ public class GptQueryBlockExecutor extends BlockExecutor {
         String subject = processContext.getConferenceContext().getSubject();
 
         StringBuilder contextBuilder = new StringBuilder();
+        contextBuilder.append("----- 이 정보들은 이번 스터디의 사회를 진행하기 위해 너가 사전에 숙지해야 할 정보들이야.\n");
         int numOfParticipant = processContext.getNumOfParticipant();
+        contextBuilder.append("<").append("이번 스터디 내 총 참여자 수=").append(numOfParticipant).append(">\n");
         for (int i=1; i<=numOfParticipant; i++) {
             String participantName = processContext.getParticipantName(i);
-            contextBuilder.append("<").append(i).append("번 참여자=").append(participantName).append(">");
+            contextBuilder.append("<").append(i).append("번 참여자=").append(participantName).append(">\n");
         }
-        contextBuilder.append("<").append("주제=").append(subject).append(">");
+        contextBuilder.append("<").append("주제=").append(subject).append(">\n");
         int phase = (int) processContext.getVariable(BlockVariable.CURRENT_PHASE.getToken());
-        contextBuilder.append("<").append("페이즈(단계)=").append(processContext).append(phase);
+        contextBuilder.append("<").append("페이즈(단계)=").append(processContext).append(phase).append(">\n");
+        contextBuilder.append("-----\n");
 
-        StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("[PrePrompt Start]").append(prePrompt).append("[PrePrompt End]");
-        queryBuilder.append("[Gpt Query History Start]").append(gptHistory).append("[Gpt Query History End]");
-        queryBuilder.append("[Context Start]").append(contextBuilder).append("[Context End]");
-        queryBuilder.append("[Current Query Start]").append(query).append("[Current Query End]");
+        StringBuilder queryBuilder = new StringBuilder("----- 이 정보들은 이번 스터디의 사회를 진행하기 위해 너가 사전에 숙지해야 할 정보들이야. -----\n");
+        queryBuilder.append("[Gpt Query History Start]\n").append(gptHistory).append("[Gpt Query History End]\n");
+        queryBuilder.append("[PrePrompt Start]\n").append(prePrompt).append("[PrePrompt End]\n");
+        queryBuilder.append("[Context Start]\n").append(contextBuilder).append("[Context End]\n");
+        queryBuilder.append("[Current Query Start]\n").append(query).append("[Current Query End]\n");
 
         String answer = externalApiCallService.gpt(queryBuilder.toString());
         processContext.addGptHistory(query, answer);
 
-        log.debug("[GPT]\n[Query]\n{}\n[Answer]\n{}", queryBuilder, answer);
+        log.debug("[GPT]\n[Query]\n{}\n\n[Answer]\n{}\n", queryBuilder, answer);
 
         return answer;
     }
